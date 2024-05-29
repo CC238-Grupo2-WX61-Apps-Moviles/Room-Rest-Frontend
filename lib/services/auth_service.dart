6d626915dd0d2a5be5706278090964b2d2249a50
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-05-29 01:25:26 -0500

    adding error prints for login

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,14 +2,21 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-    static Future<Map<String, dynamic>> login(String email, String password) async {
-        final url = Uri.parse('http://antarticdonkeys.com/login?email=$email&password=$password');
-        final response = await http.get(url);
+  static Future<Map<String, dynamic>> login(String email, String password) async {
+    final url = Uri.parse('http://antarticdonkeys.com/login?email=$email&password=$password');
+    try {
+      final response = await http.get(url);
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-        if (response.statusCode == 200) {
-            return jsonDecode(response.body);
-        } else {
-            throw Exception('Failed to login');
-        }
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception('Failed to login with status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during login: $e');
+      throw Exception('Failed to login due to network error');
     }
+  }
 }
