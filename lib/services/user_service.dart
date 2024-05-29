c63ed94770e5656922cefae66b819a3bdbe2ddb5
GabbrM <115942603+GabbrM@users.noreply.github.com>
2024-05-29 17:05:58 -0500

    feat: add user service

---
+++ lib/services/user_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class UserService{
+  static Future<Map<String, dynamic>> getUserData(int userId) async {
+    final response = await http.get(Uri.parse('http://antarticdonkeys.com/users/$userId'));
+
+    if (response.statusCode == 200 ) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load user data');
+    }
+  }
+
+}
